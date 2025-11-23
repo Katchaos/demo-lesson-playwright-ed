ac19bc828ea0e1c4a98ef98da80cf343f6abf14f
ekaterinadegtiareva <edegtiareva@evolution.com>
2025-11-23 22:35:22 +0200

    Add order creation and verification pw tests

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -1,12 +1,41 @@
-import { Locator, Page } from '@playwright/test'
+import type { Locator, Page } from '@playwright/test'
 
 export class OrderPage {
   readonly page: Page
   readonly statusButton: Locator
+  readonly userNameField: Locator
+  readonly orderButton: Locator
+  readonly userPhoneField: Locator
+  readonly userCommentField: Locator
+  readonly mainPageLink: Locator
+  readonly orderCreatedButton: Locator
+  readonly logOutButton: Locator
+  readonly userNameLengthError: Locator
+  readonly userPhoneLengthError: Locator
+  readonly userNameEmptyFieldError: Locator
+  readonly userPhoneEmptyFieldError: Locator
+
+
   // add more locators here
 
   constructor(page: Page) {
     this.page = page
     this.statusButton = page.getByTestId('openStatusPopup-button')
+    this.userNameField = page.getByTestId('username-input')
+    this.orderButton = page.getByTestId('createOrder-button')
+    this.userPhoneField = page.getByTestId('phone-input')
+    this.userCommentField = page.getByTestId('comment-input')
+    this.mainPageLink = page.getByTestId('mainPage-link')
+    this.orderCreatedButton = page.getByTestId('orderSuccessfullyCreated-popup-ok-button')
+    this.logOutButton = page.getByTestId('logout-button')
+    this.userNameLengthError = page.getByText('The field must contain at least of characters: 2')
+    this.userPhoneLengthError = page.getByText('The field must contain at least of characters: 6')
+    // this.userNameEmptyFieldError = page.getByText('The field must be filled in.')
+    this.userNameEmptyFieldError = this.userNameEmptyFieldError = page.locator('[data-name="username-input-error"]', {
+      hasText: 'The field must be filled in.'})
+    this.userPhoneEmptyFieldError = this.userPhoneEmptyFieldError = page.locator('[data-name="phone-input-error"]', {
+      hasText: 'The field must be filled in.'})
+    // this.userPhoneEmptyFieldError = page.getByText('The field must be filled in.')
+    // add more locators here
   }
 }
--- tests/ui/e2e-flow-simple.spec.ts
+++ tests/ui/e2e-flow-simple.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login-page'
-import { faker } from '@faker-js/faker/locale/ar'
+import { faker } from '@faker-js/faker/locale/en'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
 test('signIn button disabled when incorrect data inserted', async ({ page }) => {
   const authPage = new LoginPage(page)
   await authPage.open()
   await authPage.usernameField.fill(faker.lorem.word(2))
-  await authPage.passwordField.fill(faker.lorem.word(7))
+  await authPage.passwordField.fill(faker.lorem.word(2))
   await expect(authPage.signInButton).toBeDisabled()
 })
 
@@ -15,6 +15,49 @@ test('login with correct credentials and verify order creation page', async ({ p
   const authPage = new LoginPage(page)
   await authPage.open()
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
-  await orderCreationPage.statusButton.click({ force: true })
-  // verify at least few elements on the order creation page
+  await expect(orderCreationPage.userNameField).toBeVisible()
+  await expect(orderCreationPage.orderButton).toBeVisible()
+  await expect(orderCreationPage.userPhoneField).toBeVisible()
+  await expect(orderCreationPage.userCommentField).toBeVisible()
+})
+
+test('login and create order successfully', async ({ page }) => {
+  const authPage = new LoginPage(page)
+  await authPage.open()
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.userNameField.fill(faker.lorem.word(7))
+  await orderCreationPage.userPhoneField.fill(faker.lorem.word(7))
+  await orderCreationPage.orderButton.click()
+  await expect(orderCreationPage.orderCreatedButton).toBeVisible()
+})
+
+test('login and verify validation errors', async ({ page }) => {
+  const authPage = new LoginPage(page)
+  await authPage.open()
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.userNameField.fill(faker.lorem.word(1))
+  await orderCreationPage.userPhoneField.fill(faker.lorem.word(5))
+  await expect (orderCreationPage.userNameLengthError).toBeVisible()
+  await expect (orderCreationPage.userPhoneLengthError).toBeVisible()
+  await expect(orderCreationPage.orderButton).toBeDisabled()
+})
+
+test('login and verify validation errors with empty fields', async ({ page }) => {
+  const authPage = new LoginPage(page)
+  await authPage.open()
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.userNameField.fill(faker.lorem.word(1))
+  await orderCreationPage.userPhoneField.fill(faker.lorem.word(1))
+  await orderCreationPage.userNameField.fill('')
+  await orderCreationPage.userPhoneField.fill('')
+  await expect (orderCreationPage.userNameEmptyFieldError).toBeVisible()
+  await expect (orderCreationPage.userPhoneEmptyFieldError).toBeVisible()
+})
+
+test('login and log out', async ({ page }) => {
+  const authPage = new LoginPage(page)
+  await authPage.open()
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.logOutButton.click()
+  await expect (authPage.signInButton).toBeVisible()
 })
